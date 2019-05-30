07ab5a29ae2cb1d61b6c13df9645e3f2bf8c71bf
Damian <damiandn@gmail.com>
2019-05-29 21:35:42 -0400

    Started main activity Java

--- app/src/main/java/com/damiandn/omnitool/MainActivity.java
+++ app/src/main/java/com/damiandn/omnitool/MainActivity.java
@@ -1,13 +1,35 @@
 package com.damiandn.omnitool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnStartMolBio, btnStartImaging, btnStartTools;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartMolBio = (Button) findViewById(R.id.btnMolBio);
+        btnStartImaging = (Button) findViewById(R.id.btnImaging);
+        btnStartTools = (Button) findViewById(R.id.btnUtils);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+
+        btnStartMolBio.setOnClickListener(listener);
+
+
     }
 }
---
+++ app/src/main/java/com/damiandn/omnitool/MolBioMain.java
@@ -0,0 +1,4 @@
+package com.damiandn.omnitool;
+
+public class MolBioMain {
+}
